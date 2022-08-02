e52f0e09f25388a5759903f7320c2670e052609f
Vinicius <vinioliveira.s@hotmail.com>
2022-08-02 18:47:10 -0300

    Refactoring money, dollar e franc

--- Franc.java
+++ Franc.java
@@ -2,7 +2,7 @@ public class Franc extends Money{
     Franc(int amount) {      
       this.amount= amount;
     }					
-    Franc times(int multiplier)  {      
-       return new Franc(amount * multiplier);					
-    }   
+    Money times(int multiplier)  {
+        return new Franc(amount * multiplier);
+     }   
 }
